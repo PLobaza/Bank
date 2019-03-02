49a4f4694ae57d731bbb58135561ba571cd8c125
P Lobaza <lobaza.paulina@gmail.com>
2019-03-02 12:46:40 +0100

    first intelij commit

--- src/Account.java
+++ src/Account.java
@@ -1,3 +1,28 @@
 public class Account {
+ private String login;
+ private float balance;
 
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setBalance(float balance) {
+        this.balance = balance;
+    }
+
+    public float getBalance() {
+        return balance;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "login='" + login + '\'' +
+                ", balance=" + balance +
+                '}';
+    }
 }
---
+++ src/Bank.java
@@ -0,0 +1,14 @@
+public class Bank {
+    public static void main(String[] args) {
+Account account = new Account();
+account.setLogin("blabla");
+account.setBalance(1000);
+
+
+/*account.getLogin();
+account.getBalance();*/
+
+        System.out.println(account.toString());
+    }
+
+}
